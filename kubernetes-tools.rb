07732b94762d32496dd0dc17d998ae530c486e2c
Shawn <shawnxlw@users.noreply.github.com>
2018-10-16 01:17:27 +1100

    updated ktools to v2.0.0

--- kubernetes-tools.rb
+++ kubernetes-tools.rb
@@ -1,11 +1,9 @@
 class KubernetesTools < Formula
   desc "Kubernetes tools is a set of programs that help you operate your Kubernetes clusters more efficiently."
   homepage "https://github.com/shawnxlw/kubernetes-tools"
-  url "https://github.com/shawnxlw/kubernetes-tools/archive/v1.2.0.tar.gz"
-  sha256 "b99d2641c096c0bc94b8ff32be71304ba5a6561b4b8dbc544051e5f2db6cff54"
+  url "https://github.com/shawnxlw/kubernetes-tools/archive/v2.0.0.tar.gz"
+  sha256 "e0680aa0ae87ea6868936bd0b17bc7456b39355ed420211d4518448301912061"
 
-    depends_on "tmux"
-    depends_on "tmux-cssh"
     depends_on "jq"
     depends_on "coreutils"
 
@@ -14,15 +12,11 @@ class KubernetesTools < Formula
       bin.install "bin/kctx"
       bin.install "bin/kns"
       bin.install "bin/kpod"
-      bin.install "bin/kds"
-      bin.install "bin/klogs"
       bin.install "bin/kexec"
-      bin.install "bin/kcp"
-      bin.install "bin/kbak"
+      bin.install "bin/kcopy"
       bin.install "bin/busybox"
       bin.install "bin/curl"
       prefix.install Dir["completion"]
-      prefix.install Dir["helper"]
     end
 
 end
